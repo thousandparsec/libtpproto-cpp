63600238a2d0a579b8c17c190a87d94ed6dbe230
Lee Begg <lee@thousandparsec.net>
2004-10-29 13:11:26 +0000

    Might as well visit all current object types.

--- tpproto/objectvisitor.h
+++ tpproto/objectvisitor.h
@@ -4,10 +4,21 @@
 namespace TPProto{
 
   class Object;
+  class Universe;
+  class Galaxy;
+  class StarSystem;
+  class Planet;
+  class Fleet;
   
   class ObjectVisitor{
   public:
     virtual ~ObjectVisitor();
+
+    virtual void visit(Universe* ob) = 0;
+    virtual void visit(Galaxy* ob) = 0;
+    virtual void visit(StarSystem* ob) = 0;
+    virtual void visit(Planet* ob) = 0;
+    virtual void visit(Fleet* ob) = 0;
 
     virtual void visit(Object* ob) = 0;
     
