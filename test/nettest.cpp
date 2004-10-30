db41466b50ced27058d8f214c926d392c800115e
Lee Begg <lee@thousandparsec.net>
2004-10-30 03:29:58 +0000

    Test message boards.

--- test/nettest.cpp
+++ test/nettest.cpp
@@ -3,6 +3,7 @@
 #include <tpproto/framecodec.h>
 #include <tpproto/tcpsocket.h>
 #include <tpproto/object.h>
+#include <tpproto/board.h>
 
 #include "downloadprintvisitor.h"
 
@@ -58,6 +59,26 @@ int main(int argc, char** argv){
 	  
 	  std::cout << "Finished Visitor test, status: " << myfc->getStatus() << std::endl;
 
+	  std::cout << "Starting Board test" << std::endl;
+
+	  Board* myboard = myfc->getPersonalBoard();
+
+	  std::cout << "Got personal board, status " << myfc->getStatus() << std::endl;
+
+	  if(myboard != NULL){
+
+	    std::cout << "Personal board, id: " << myboard->getId() << std::endl << "name: " << myboard->getName()
+		      << std::endl << "Desc: " << myboard->getDescription() << std::endl << "Num messages: "
+		      << myboard->numMessages() << std::endl;
+
+	    std::cout << "Board test finished" << std::endl;
+	    
+	    delete myboard;
+
+	  }else{
+	    std::cout << "Board test failed, board is NULL, status " << myfc->getStatus() << std::endl;
+	    status = 5;
+	  }
 	}else{
 	  std::cout << "Failed to get Universe Object, status" << myfc->getStatus() << std::endl;
 	  status = 4;
