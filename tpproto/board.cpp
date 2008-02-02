83bb64bf2dd446ee5e78c8074c9d39a6af8ea247
Lee Begg <llnz@paradise.net.nz>
2008-02-03 00:17:44 +1300

    Fixed Board to work with strings from Buffer. Also added license information.

--- tpproto/board.cpp
+++ tpproto/board.cpp
@@ -1,4 +1,22 @@
-
+/*  Board - Frame send by the server to tell the client about a board.
+ *
+ *  Copyright (C) 2005, 2006, 2008  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 #include <cassert>
 
 #include "buffer.h"
@@ -35,12 +53,8 @@ namespace TPProto{
   */
   bool Board::unpackBuffer(Buffer* buf){
     id = buf->unpackInt();
-    char* temp = buf->unpackString();
-    name = temp;
-    delete[] temp;
-    temp = buf->unpackString();
-    desc = temp;
-    delete[] temp;
+    name = buf->unpackString();
+    desc = buf->unpackString();
 
     nummess = buf->unpackInt();
     if(protoVer >= 3){
