761cc10a2e18bab7b5a3d27929bc7d1fbecce543
Lee Begg <llnz@paradise.net.nz>
2008-02-03 00:29:45 +1300

    Fixed ListParamter and StringParameter to work with strings to/from Buffer. Also added license info to all 4 files.

--- tpproto/listparameter.cpp
+++ tpproto/listparameter.cpp
@@ -1,4 +1,22 @@
-
+/*  ListParameter - OrderParameter that presents a list of selectable things.
+ *
+ *  Copyright (C) 2005, 2006, 2008  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
 #include "buffer.h"
 #include "orderparametervisitor.h"
@@ -54,10 +72,9 @@ namespace TPProto{
     int numsa = buf->unpackInt();
     for(int i = 0; i < numsa; i++){
       unsigned int id = buf->unpackInt();
-      char* iname = buf->unpackString();
+      std::string iname = buf->unpackString();
       unsigned int maxnum = buf->unpackInt();
       selectable[id] = SelectableElement(iname, maxnum);
-      delete iname;
     }
     int nums = buf->unpackInt();
     for(int i = 0; i < nums; i++){
