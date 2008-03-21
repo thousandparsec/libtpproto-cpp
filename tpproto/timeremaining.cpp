c88019e6c8f03a16de3917617ccfb81a92c1ac96
Lee Begg <llnz@paradise.net.nz>
2008-03-21 18:19:04 +1300

    Added reason code to TimeRemaining frame for TP04.

--- tpproto/timeremaining.cpp
+++ tpproto/timeremaining.cpp
@@ -1,3 +1,22 @@
+/*  TimeRemaining Frame
+ *
+ *  Copyright (C) 2005-2006, 2008  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
 #include <cassert>
 
@@ -32,6 +51,9 @@ namespace TPProto{
   */
   bool TimeRemaining::unpackBuffer(Buffer* buf){
     time = buf->unpackInt();
+    if(protoVer >= 4){
+      reason = buf->unpackInt();
+    }
     type = ft02_Time_Remaining;
     return true;
   }
@@ -39,8 +61,12 @@ namespace TPProto{
   /*! \brief Gets the time remaining before the end of turn.
     \return The time in seconds before the end of turn.
   */
-  int TimeRemaining::getTimeRemaining(){
+  uint32_t TimeRemaining::getTimeRemaining(){
     return time;
+  }
+
+  uint32_t TimeRemaining::getReasonCode(){
+    return reason;
   }
 
 }
--- tpproto/timeremaining.h
+++ tpproto/timeremaining.h
@@ -1,13 +1,45 @@
 #ifndef TPPROTO_TIMEREMAINING_H
 #define TPPROTO_TIMEREMAINING_H
+/*  TimeRemaining Frame
+ *
+ *  Copyright (C) 2005-2006, 2008  Lee Begg and the Thousand Parsec Project
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 
 /*! \file
   \brief Declares the TimeRemaining Frame.
 */
 
+#include <stdint.h>
 #include <tpproto/frame.h>
 
 namespace TPProto{
+  
+  /*! \brief The enum of known reason codes for TimeRemaining Frames.
+  */
+  enum TimeRemainingReasons {
+    TRR_Requested = 0,
+    TRR_Timer_Started = 1,
+    TRR_Advanced_Warning = 2,
+    TRR_All_Finished_EOT = 3,
+    TRR_Threshold_Finished = 4,
+    TRR_EOT_Started = 5
+  };
 
   /*! \brief Notification of the time remaining before the end of turn.
    */
@@ -19,10 +51,12 @@ namespace TPProto{
     void packBuffer(Buffer* buf);
     bool unpackBuffer(Buffer* buf);
 
-    int getTimeRemaining();
+    uint32_t getTimeRemaining();
+    uint32_t getReasonCode();
 
   private:
-    int time;
+    uint32_t time;
+    uint32_t reason;
 
   };
 
